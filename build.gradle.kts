ca6b07f4e8b4e1551e0ffe22a5661e506d9cf411
Ruslan Gainutdinov <ruslanfg@gmail.com>
2020-08-04 23:35:54 +0300

    Buildable API doc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.moowork.gradle.node.npm.NpmTask
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 group = "lunchtime"
@@ -5,8 +6,8 @@ version = "0.1-SNAPSHOT"
 
 plugins {
     kotlin("jvm") version "1.3.72" apply true
+    id("com.github.node-gradle.node") version "2.2.4" apply true
     application
-    id("io.kotless") version "0.1.5" apply true
 }
 
 repositories {
@@ -20,23 +21,22 @@ sourceSets {
     }
 }
 
+tasks.register<NpmTask>("npmBuild") {
+    setArgs(listOf("run", "build"))
+}
+
 tasks.withType<KotlinJvmCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
         languageVersion = "1.3"
         apiVersion = "1.3"
-        // Unlock experimental ktor features
-        freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
     }
 }
 
 dependencies {
-    //implementation("io.ktor:ktor-server-core:1.3.2")
+    implementation("io.ktor:ktor-server-core:1.3.2")
     implementation("io.ktor:ktor-server-netty:1.3.2")
-    implementation("io.kotless:ktor-lang:0.1.5") {
-        // Exclude log4j building
-        exclude(group = "org.slf4j", module = "slf4j-log4j12")
-    }
+    implementation("io.ktor:ktor-jackson:1.3.2")
     implementation("ch.qos.logback:logback-classic:1.2.3")
 }
 
--- src/main/kotlin/org/ruslan/lunchtime/Entrypoint.kt
+++ src/main/kotlin/org/ruslan/lunchtime/Entrypoint.kt
@@ -1,44 +1,41 @@
 package org.ruslan.lunchtime
 
-import io.kotless.dsl.ktor.Kotless
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.response.respondText
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.content.resource
+import io.ktor.http.content.static
+import io.ktor.jackson.jackson
+import io.ktor.response.respondRedirect
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.netty.EngineMain
-import org.slf4j.LoggerFactory
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
 }
 
-class Server : Kotless() {
-    override fun prepare(app: Application) {
-        app.main()
-    }
-}
-
-val log = LoggerFactory.getLogger("main")
-
 fun Application.main() {
-    log.info("Starting main module") // , environment: " + envKind)
+    install(ContentNegotiation) {
+        jackson {
+            setSerializationInclusion(JsonInclude.Include.NON_NULL)
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
     routing {
+        static {
+            resource("favicon.ico")
+            resource("redoc.html", "generated/redoc.html")
+            resource("openapi.yaml")
+            resource("example-request.json")
+            resource("example-response.json")
+        }
         get("/") {
-            call.respondText("Hello World!")
+            call.respondRedirect("redoc.html")
         }
     }
 }
-
-/**
- * FIXME: Marked as experimental, however, referenced in the official docs as not experimental.
- * See: https://ktor.io/servers/configuration/environments.html
- **/
-/*
-@OptIn(KtorExperimentalAPI::class)
-fun Application.env(key: String) = environment.config.property(key).getString()
-
-val Application.envKind get() = env("ktor.environment")
-val Application.isDev get() = envKind == "dev"
-val Application.isProd get() = envKind != "dev"
-*/
